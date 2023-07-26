c6db9dd4e2fc25a1f94a0f3e6f3a97d33e9beedb
yangj132 <yangjie.yang@mail.utoronto.ca>
2023-07-25 21:12:25 -0700

    First Commit

---
+++ WebAPI/WebAPI/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI4.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// 配置 CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// Json Serverlizer
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(),"Photos")),
+    RequestPath="/Photos"
+});
+
+app.MapControllers();
+
+app.Run();
